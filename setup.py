e469a61d30eaf0e39bd8e3a46f17717f9cfb0911
Bastian Venthur <venthur@debian.org>
2010-07-29 10:33:50 +0200

    Added setup.py for distutils.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="python-debianbts",
+        version="foo",
+        description="Python interface to Debians Bug Tracking System",
+        author="Bastian Venthur",
+        author_email="venthur@debian.org",
+        py_modules=["debianbts"],
+        package_dir={"": "src"},
+        )
+
