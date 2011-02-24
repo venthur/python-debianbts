f2e987636a21ddeb12fe3e46ce0c3073d2640088
Fladischer Michael <FladischerMichael@fladi.at>
2011-02-24 11:45:04 +0100

    Add some additional meta information for PyPi.

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
-#!/usr/bin/env python
-
 from distutils.core import setup
 
-setup(name="python-debianbts",
-        version="1.10",
-        description="Python interface to Debians Bug Tracking System",
-        author="Bastian Venthur",
-        author_email="venthur@debian.org",
-        py_modules=["debianbts"],
-        package_dir={"": "src"},
-        )
-
+setup(
+    name='python-debianbts',
+    version='1.10',
+    description='Python interface to Debians Bug Tracking System',
+    keywords='debian, soap, bts',
+    author='Bastian Venthur',
+    author_email='venthur@debian.org',
+    url='https://github.com/venthur/python-debianbts',
+    license='GPL2',
+    package_dir = {'': 'src'},
+    py_modules = ['debianbts'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python",
+        "Topic :: Communications",
+        "Topic :: Software Development :: Bug Tracking",
+    ],
+)
