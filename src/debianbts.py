aca7ac258a49dc5a0e268e3889eb3d2d218ad876
James McCoy <jamessan@debian.org>
2015-07-13 20:27:35 -0400

    get_status: Process input in batches
    When bugs.debian.org was upgraded to Jessie, resources limits were set
    in the Apache configuration.  This means that process of get_status
    requests for a large number of bugs will cause the process to get
    killed, due to exceeding the resource limits.
    
    Processing the input to get_status in batches avoids hitting the
    resource limits.

--- src/debianbts.py
+++ src/debianbts.py
@@ -179,20 +179,28 @@ class Bugreport(object):
         return val
 
 
-def get_status(*nr):
+def get_status(*nrs):
     """Returns a list of Bugreport objects."""
-    reply = server.get_status(*nr)
     # If we called get_status with one single bug, we get a single bug,
     # if we called it with a list of bugs, we get a list,
-    # No available bugreports returns an enmpy list
+    # No available bugreports returns an empty list
     bugs = []
-    if not reply:
-        pass
-    elif type(reply[0]) == type([]):
-        for elem in reply[0]:
-            bugs.append(_parse_status(elem))
-    else:
-        bugs.append(_parse_status(reply[0]))
+    def parse(n):
+        if not n:
+            return []
+        elif type(reply[0]) == type([]):
+            return [_parse_status(elem) for elem in reply[0]]
+        else:
+            return [_parse_status(reply[0])]
+    # Process the input in batches to avoid hitting resource limits on the BTS
+    for nr in nrs:
+        if isinstance(nr, list):
+            for i in range(0, len(nr), 500):
+                reply = server.get_status(nr[i:i+500])
+                bugs.extend(parse(reply))
+        else:
+            reply = server.get_status(nr)
+            bugs.extend(parse(reply))
     return bugs
 
 
