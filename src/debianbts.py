venthur/python-debianbts

commit aca7ac258a49dc5a0e268e3889eb3d2d218ad876

get_status: Process input in batches
When bugs.debian.org was upgraded to Jessie, resources limits were set
in the Apache configuration.  This means that process of get_status
requests for a large number of bugs will cause the process to get
killed, due to exceeding the resource limits.

Processing the input to get_status in batches avoids hitting the
resource limits.

src/debianbts.py
@@ -179,20 +179,28 @@ class Bugreport(object):
         return val
 
 
-def get_status(*nr):
+def get_status(*nrs):
     """Returns a list of Bugreport objects."""
-    reply = server.get_status(*nr)
     # If we called get_status with one single bug, we get a single bug,
     # if we called it with a list of bugs, we get a list,
-    # No available bugreports returns an enmpy list
+    # No available bugreports returns an empty list
     bugs = []
-    if not reply:
-        pass
-    elif type(reply[0]) == type([]):
-        for elem in reply[0]:
-            bugs.append(_parse_status(elem))
-    else:
-        bugs.append(_parse_status(reply[0]))
+    def parse(n):
+        if not n:
+            return []
+        elif type(reply[0]) == type([]):
+            return [_parse_status(elem) for elem in reply[0]]
+        else:
+            return [_parse_status(reply[0])]
+    # Process the input in batches to avoid hitting resource limits on the BTS
+    for nr in nrs:
+        if isinstance(nr, list):
+            for i in range(0, len(nr), 500):
+                reply = server.get_status(nr[i:i+500])
+                bugs.extend(parse(reply))
+        else:
+            reply = server.get_status(nr)
+            bugs.extend(parse(reply))
     return bugs
 
 

commit 8efc42ca5c5dfe201bfd84626ddb19fe66cb9f4d

get_status: Flatten input before sending requests
aca7ac258a49dc5a0e268e3889eb3d2d218ad876 introduced a performance
regression (as reported in Debian bug #795198) for typical inputs, since
every bug's status would be requested individually.

Flattening the input to a single list before performing the batch
requests ensures we're not performing unnecessary requests.

Signed-off-by: James McCoy <jamessan@debian.org>

src/debianbts.py
@@ -194,15 +194,16 @@ def get_status(*nrs):
             return [_parse_status(elem) for elem in reply[0]]
         else:
             return [_parse_status(reply[0])]
+    L = []
     # Process the input in batches to avoid hitting resource limits on the BTS
     for nr in nrs:
         if isinstance(nr, list):
-            for i in range(0, len(nr), BATCH_SIZE):
-                reply = server.get_status(nr[i:i+BATCH_SIZE])
-                bugs.extend(parse(reply))
+            L.extend(nr)
         else:
-            reply = server.get_status(nr)
-            bugs.extend(parse(reply))
+            L.append(nr)
+    for i in range(0, len(L), BATCH_SIZE):
+        reply = server.get_status(L[i:i+BATCH_SIZE])
+        bugs.extend(parse(reply))
     return bugs
 
 