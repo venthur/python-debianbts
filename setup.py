9667f316ae6e2d90c4bdc38c1e28d6e642bbbfc7
Bastian Venthur <venthur@debian.org>
2019-01-12 15:16:44 +0100

    removed mock from requirements

--- setup.py
+++ setup.py
@@ -24,7 +24,6 @@ setup(
           'pytest',
           'pytest-cov',
           'flake8',
-          'mock;python_version<"3.0"',
       ]
     },
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
